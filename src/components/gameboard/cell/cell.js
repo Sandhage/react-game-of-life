4297dc44a8fc23916e8e2d16f08ff81040ba0dfa
Slo <sloane.sandhage@gmail.com>
2023-02-23 11:48:23 -0600

    initialized gameboard and cell components

---
+++ src/components/gameboard/cell/cell.js
@@ -0,0 +1,9 @@
+const Cell = () => {
+	return (
+		<>
+			// Cell code goes here. Logic will take in information about the cell this represents in the 2D array and wether or not it is "alive" or "dead" and update CSS accordingly.
+		</>
+	);
+}
+
+export default Cell;
---
+++ src/components/gameboard/gameboard.js
@@ -0,0 +1,9 @@
+const Gameboard = () => {
+	return (
+		<>
+			// Gameboard goes here. Planning to use CSS grid.
+		</>
+	);
+}
+
+export default Gameboard;
